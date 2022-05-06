002d001fb03ce97f8aa9229edc79d09f7552bbab
paula1309 <paulaalexandra1903@hotmail.com>
2022-05-06 18:50:37 -0300

    atualizando decodificador, adicionando botão de copiar

--- Decodificador-de-textos/assets/script.js
+++ Decodificador-de-textos/assets/script.js
@@ -29,19 +29,98 @@ function criptografar() {
 
     var textoJunto = textoSeparado.join("");
 
-    var elementoPai = document.getElementsByClassName("exibir-texto");
-    var elementoFilho = elementoPai.lastElementChild;
+    var troca = document.getElementById("exibir-texto");
+    troca.innerHTML = '<p>' + textoJunto + '</p>' + '<button> Copiar </button>';
     
-    console.log(elementoFilho);
+    var botao = troca.lastElementChild;
+    botao.className = "descriptografar";
+    botao.id = "copiar";
 
-    while (elementoFilho) {
-        elementoPai.removeChild(elementoFilho);
+    troca.style.display = "flex";
+    troca.style.flexDirection = "column";
+    troca.style.justifyContent = "space-between";
+
+    botao.style.marginLeft = "40px";
+    botao.style.marginBottom = "40px";
+
+    var p = troca.firstElementChild;
+    p.id = "texto-criptografado"
+    p.style.fontSize = 24 + "px";
+    p.style.paddingTop = 50 + "px";
+    p.style.lineHeight = 36 + "px";
+
+    botao.addEventListener('click', copiarBotao)
+
+    function copiarBotao() {
+
+        var textoCopia = p.textContent;
+
+        console.log(textoCopia)
+
+        navigator.clipboard
+            .writeText(textoCopia)
+            .then(() => {
+                alert("successfully copied");
+            })
+            .catch(() => {
+                alert("something went wrong");
+            });
     }
-    console.log(elementoPai);
 
-    return console.log(textoJunto);
 }
 
 function descriptografar() {
-    return console.log("clicou 2");
+    
+    var textoCriptografado = texto.value;
+
+    var letraA = textoCriptografado.replace(/ai/gi, 'a');
+    var letraE = letraA.replace(/enter/gi, 'e');
+    var letraI = letraE.replace(/imes/gi, 'i');
+    var letraO = letraI.replace(/ober/gi, 'o');
+    var letraU = letraO.replace(/ufat/gi, 'u');
+
+    var troca = document.getElementById("exibir-texto");
+    troca.innerHTML = '<p>' + letraU + '</p>' + '<button> Copiar </button>';
+
+    var p = troca.firstElementChild;
+    p.id = "texto-criptografado"
+    p.style.fontSize = 24 + "px";
+    p.style.paddingTop = 50 + "px";
+    p.style.lineHeight = 36 + "px";
+
+    var botao = troca.lastElementChild;
+    botao.className = "descriptografar";
+    botao.id = "copiar";
+
+    troca.style.display = "flex";
+    troca.style.flexDirection = "column";
+    troca.style.justifyContent = "space-between";
+
+    botao.style.marginLeft = "40px";
+    botao.style.marginBottom = "40px";
+
+    var p = troca.firstElementChild;
+    p.id = "texto-criptografado"
+    p.style.fontSize = 24 + "px";
+    p.style.paddingTop = 50 + "px";
+    p.style.lineHeight = 36 + "px";
+
+    botao.addEventListener('click', copiarBotao)
+
+    function copiarBotao() {
+
+        var textoCopia = p.textContent;
+
+        console.log(textoCopia)
+
+        navigator.clipboard
+            .writeText(textoCopia)
+            .then(() => {
+                alert("copiado com sucesso!!");
+            })
+            .catch(() => {
+                alert("algo deu errado!!");
+            });
+    }
+
 }
